7d816ef7cc787b85b10b1df42a0bfe131ce06b8d
Bryce Chamberlain <superchordate@gmail.com>
2024-07-16 11:34:41 -0500

    Allow writing of xlsx.

--- R/w.R
+++ R/w.R
@@ -1,7 +1,7 @@
 #' Write
 #'
-#' Improved write function. Writes to csv without row names and automatically adds .csv to the file name if it isn't there already. Changes to .csv if another extension is passed.
-#' Easier to type than write.csv(row.names=F).
+#' Improved write function. Writes to csv or excel without row names and automatically adds .csv to the file name if it isn't there already. Changes to .csv if a non-xlsx extension is passed.
+#' Easier to type than write.csv(row.names=F) or write.excel().
 #' Author: Bryce Chamberlain. Tech reveiw: Maria Gonzalez.
 #'
 #' @param x Data frame to write to file.
@@ -19,11 +19,15 @@
 #' # cleanup.
 #' file.remove(path)
 w <- function(x, filename = 'out', row.names = FALSE, na = ''){
-  
-  # force csv extension.
-  filename <- paste0(gsub( '[.][a-z]+$','', filename, ignore.case = TRUE), '.csv' ) 
-  
-  # use faster fwrite. 
-  data.table::fwrite(x = x, file = filename, row.names = row.names, na = na)
+
+  ext = stringr::str_extract(filename, '[.][^.]+$')
+  filename = gsub('[.][^.]+$', '', filename)
+
+  if(is.na(ext) || ext %ni% c('.csv', '.xlsx')) ext = '.csv'
+  if(ext == '.csv'){
+    data.table::fwrite(x = x, file = paste0(filename, ext), row.names = row.names, na = na)
+  } else if(ext == '.xlsx'){
+    openxlsx::write.xlsx(x = x, file = paste0(filename, ext), rowNames = FALSE)
+  }
   
 }
--- tests/testthat/test_w.R
+++ tests/testthat/test_w.R
@@ -1,13 +1,19 @@
 test_that("w works as expected", {
     
     # Write a file.
-    w( cars, paste0( tempdir(), '/cars' ) )
+    w(cars, paste0(tempdir(), '/cars'))
 
     # Read the file and delete it.
-    t = read.csv( paste0( tempdir(), '/cars.csv' ) )
-    file.remove( paste0( tempdir(), '/cars.csv' ) )
+    t = read.csv(paste0(tempdir(), '/cars.csv'))
+    file.remove(paste0(tempdir(), '/cars.csv'))
 
     # Verify it is as expected.
-    expect_equal( t, cars )
+    expect_equal(t, cars)
+    
+    # now in excel. 
+    w(cars, paste0(tempdir(), '/cars.xlsx'))
+    t = openxlsx::read.xlsx(paste0(tempdir(), '/cars.xlsx'))
+    file.remove(paste0(tempdir(), '/cars.xlsx'))
+    expect_equal(t, cars)
     
 })
